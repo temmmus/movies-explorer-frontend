1eb5fb64dafba54311327b19065886f64dd8c9c0
temmmus <temmmus@gmail.com>
2022-07-15 01:09:36 +0500

    частичные правки верстки под разрешение 320px

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -7,11 +7,11 @@ function AboutProject() {
             <SectionTitle text="О проекте"/>
             <div className="about__content">
                 <article className="about__article">
-                    <h3 className="about__subtitle">Дипломный проект включал 5 этапов</h3>
+                    <h3 className="about__title">Дипломный проект включал 5 этапов</h3>
                     <p className="about__text">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
                 </article>
                 <article className="about__article">
-                    <h3 className="about__subtitle">На выполнение диплома ушло 5 недель</h3>
+                    <h3 className="about__title">На выполнение диплома ушло 5 недель</h3>
                     <p className="about__text">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
                 </article>
             </div>
